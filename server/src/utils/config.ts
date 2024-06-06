b9044ec19292307776c194d5b706f6cf9e753107
Roberto <robert18carl@gmail.com>
2024-06-06 11:30:28 -0400

    cambios en las variables de entorno

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -10,7 +10,7 @@ export const socketMsg = {
 
 export const PORT = process.env.PORT || 5000
 export const DB_URL = process.env.DB_URL
-export const DB_HOST  = process.env.DB_LOCALHOST ||"localhost"
+export const DB_HOST  = process.env.DB_HOST ||"localhost"
 export const DB_PORT:any = process.env.DB_PORT || 5432
 export const DB_USER = process.env.DB_USER || "postgres"
 export const DB_PASSWORD = process.env.DB_PASSWORD || "1331"
